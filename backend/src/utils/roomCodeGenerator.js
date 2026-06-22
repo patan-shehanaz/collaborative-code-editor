dca1a779ae655d0e9405e37e28a0647f244d7f94
sriteja-it <sritejamainampati@gmail.com>
2026-06-22 09:04:50 +0530

    Update backend

--- backend/src/utils/roomCodeGenerator.js
+++
@@ -1,6 +0,0 @@
-const { nanoid } = require('nanoid');
-
-// Short, URL-friendly, shareable room code, e.g. "x7Qa2K9d"
-const generateRoomCode = () => nanoid(8);
-
-module.exports = generateRoomCode;
